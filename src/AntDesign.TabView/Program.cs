dd14b87571c6787f49080f9b018e32cb480a85c3
James Yeung <shunjiey@hotmail.com>
2021-07-05 19:09:48 +0800

    add the demo for reuse tabs

---
+++ src/AntDesign.TabView/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AntDesign.TabView
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
